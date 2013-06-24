32b8a7539aa78bb9ca522a90bbfa43893026ab37
Jonathan Thuau <jonathan.thuau@gmail.com>
2013-06-25 00:10:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Share
+
+Share links with Laravel 4
+
+[![Build Status](https://travis-ci.org/thujohn/share-l4.png?branch=master)](https://travis-ci.org/thujohn/share-l4)
+
+
+## Installation
+
+Add `thujohn/share` to `composer.json`.
+
+    "thujohn/share": "dev-master"
+    
+Run `composer update` to pull down the latest version of Share.
+
+Now open up `app/config/app.php` and add the service provider to your `providers` array.
+
+    'providers' => array(
+        'Thujohn\Share\ShareServiceProvider',
+    )
+
+Now add the alias.
+
+    'aliases' => array(
+        'Share' => 'Thujohn\Share\ShareFacade',
+    )
+
+
+## Usage
+
+Get a link (example with Twitter)
+
+	Route::get('/', function()
+	{
+		return Share::load('http://www.example.com', 'My example')->twitter();
+	});
+
+Returns a string :
+
+	https://twitter.com/intent/tweet?url=http%3A%2F%2Fwww.example.com&text=Link+description
+
+
+Get many links
+
+	Route::get('/', function()
+	{
+		return Share::load('http://www.example.com', 'Link description')->services('facebook', 'gplus', 'twitter');
+	});
+
+Returns an array :
+
+	{"facebook":"https:\/\/www.facebook.com\/sharer\/sharer.php?u=http%3A%2F%2Fwww.example.com&title=Link+description","gplus":"https:\/\/plus.google.com\/share?url=http%3A%2F%2Fwww.example.com","twitter":"https:\/\/twitter.com\/intent\/tweet?url=http%3A%2F%2Fwww.example.com&text=Link+description"}
+
+
+## Services availables
+- Delicious : delicious
+- Digg : digg
+- Evernote : evernote
+- Facebook : facebook
+- Gmail : gmail
+- Google Plus : gplus
+- LinkedIn : linkedin
+- Pinterest : pinterest
+- Reddit : reddit
+- Scoop.it : scoopit
+- Springpad : springpad
+- Tumblr : tumblr
+- Twitter : twitter
+- Viadeo : viadeo
---
+++ src/Thujohn/Share/Share.php
@@ -0,0 +1,85 @@
+<?php namespace Thujohn\Share;
+
+class Share {
+	protected $link;
+	protected $text;
+
+	public function load($link, $text = ''){
+		$this->link = urlencode($link);
+		$this->text = urlencode($text);
+
+		return $this;
+	}
+
+	public function services(){
+		$services = func_get_args();
+
+		$return = array();
+
+		if ($services){
+			foreach ($services as $service){
+				if (method_exists('Thujohn\Share\Share', $service)){
+					$return[$service] = $this->$service();
+				}
+			}
+		}
+
+		return $return;
+	}
+
+	public function delicious(){
+		return 'https://delicious.com/post?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function digg(){
+		return 'http://www.digg.com/submit?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function evernote(){
+		return 'http://www.evernote.com/clip.action?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function facebook(){
+		return 'https://www.facebook.com/sharer/sharer.php?u='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function gmail(){
+		return 'https://mail.google.com/mail/?view=cm&fs=1&to&ui=2&tf=1&su='.$this->link.(($this->text) ? '&body='.$this->text : '');
+	}
+
+	public function gplus(){
+		return 'https://plus.google.com/share?url='.$this->link;
+	}
+
+	public function linkedin(){
+		return 'http://www.linkedin.com/shareArticle?mini=true&url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function pinterest(){
+		return 'http://pinterest.com/pin/create/button/?url='.$this->link.(($this->text) ? '&description='.$this->text : '');
+	}
+
+	public function reddit(){
+		return 'http://www.reddit.com/submit?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function scoopit(){
+		return 'http://www.scoop.it/oexchange/share?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+
+	public function springpad(){
+		return 'https://springpadit.com/s?type=lifemanagr.Bookmark&url='.$this->link.(($this->text) ? '&name='.$this->text : '');
+	}
+
+	public function tumblr(){
+		return 'http://www.tumblr.com/share?v=3&u='.$this->link.(($this->text) ? '&t='.$this->text : '');
+	}
+
+	public function twitter(){
+		return 'https://twitter.com/intent/tweet?url='.$this->link.(($this->text) ? '&text='.$this->text : '');
+	}
+
+	public function viadeo(){
+		return 'http://www.viadeo.com/?url='.$this->link.(($this->text) ? '&title='.$this->text : '');
+	}
+}
